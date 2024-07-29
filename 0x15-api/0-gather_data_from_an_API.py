4a82b886c19df7cf6ce29ed578141b64d6f317e6
freakinlight <mazouznour0@gmail.com>
2024-07-29 12:01:50 +0000

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,26 +2,26 @@
 import requests
 import sys
 
+
 def fetch_data(employee_id):
     # URL to the API endpoint
     api_url = 'https://jsonplaceholder.typicode.com'
-    
     # Fetching user data
     user_url = f'{api_url}/users/{employee_id}'
     user_response = requests.get(user_url)
     user = user_response.json()
-    
     # Fetching todos data
     todos_url = f'{api_url}/todos?userId={employee_id}'
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
-    
     # Calculating TODO list progress
     total_tasks = len(todos)
     completed_tasks = len([task for task in todos if task['completed']])
-
     # Printing the progress
-    print(f"Employee {user['name']} is done with tasks({completed_tasks}/{total_tasks}):")
+    print(
+        f"Employee {user['name']} is done with tasks("
+        f"{completed_tasks}/{total_tasks}):"
+    )
     for task in todos:
         if task['completed']:
             print(f"\t {task['title']}")
