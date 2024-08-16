91b28df958dd225faba067e8d871c4916b305ff7
freakinlight <mazouznour0@gmail.com>
2024-08-16 13:20:52 +0200

    Fix Apache 500 error by correcting permissions

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,14 @@
-# 0-strace_is_your_friend.pp
 # This Puppet manifest fixes an issue causing a 500 Internal Server Error on Apache.
 
-# Example: Fix permissions for the web root directory
 file { '/var/www/html':
-  ensure  => 'directory',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
+  ensure => 'directory',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
 }
 
-# Ensure Apache is running
 service { 'apache2':
   ensure  => 'running',
   enable  => true,
   require => File['/var/www/html'],
 }
-
