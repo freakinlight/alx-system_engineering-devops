6f6be98117f7962391da2c7315140196839c8052
freakinlight <mazouznour0@gmail.com>
2024-08-22 11:35:35 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,9 @@
-# Increases how much of traffic an Nginx server can handle.
-
-# Increase Nginx file descriptor limit
+# Puppet manifest to increase traffic an Nginx server can handle.
 exec { 'adjust-nginx-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx service
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
