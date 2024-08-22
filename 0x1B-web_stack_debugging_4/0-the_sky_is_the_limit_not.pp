092a41333c3b3165fdc98a1083cac03852e86f06
freakinlight <mazouznour0@gmail.com>
2024-08-22 09:41:17 +0000

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increase Nginx file descriptor limit
+exec { 'adjust-nginx-ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx service
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
